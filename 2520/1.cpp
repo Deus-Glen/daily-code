eee710e449c95e9edd901e295d182c23c712cce4
14973 <narrateforone@gmail.com>
2021-11-15 19:14:51 +0800

    1

--- 2520/1.cpp
+++ 2520/1.cpp
@@ -7,29 +7,10 @@
 using namespace std;
 typedef long long ll;
 
-double a[40][40];
-int b[43];
-double hai(int x, int y, int z)
-{
-	double p = (x + y + z) / 2.0;
-	return sqrt(p * (p - x) * (p - y) * (p - z));
-}
+
 int main() 
 {
-	int n, sum = 0;
-	for (int i = 1;i <= n;i++)
-	{
-		cin >> b[i];
-		sum += b[i];
-	}
-	for (int k = 1;k <= n;k++)
-	{
-		for (int i = sum / 2;i > 0;i--)
-			for (int j = sum / 2;j > 0;j--)
-			{
-				
-			}
-	}
+	
 
 
 	return 0;
